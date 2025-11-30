e6ecde44705d4464c73f1f106cf138c03f304ef1
domenicomazzella-cloud <domenicomazzella@alchiardiluna.it>
2025-12-01 00:31:46 +0100

    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,2 @@
-export const APP_VERSION = '1.7.0-FINAL';
-export const CHANGELOG = [
-  { version: '1.7.0', date: '2023-12-01', changes: ['Fix Root Structure'] }
-];
+export const APP_VERSION = '3.0.0-REBOOT';
+export const CHANGELOG = [{version: '3.0.0', date: 'NOW', changes: ['Reset Totale Sistema']}];
